79767fdc49a6920655e316f6c5ace0bd2d48f55d
Marie Tornetto <marietornetto@gmail.com>
2023-06-03 02:52:31 +0000

    update form fix

--- server/api/auth.js
+++ server/api/auth.js
@@ -48,9 +48,37 @@ app.get("/", isLoggedIn, (req, res, next) => {
 app.put("/", isLoggedIn, async (req, res, next) => {
   try {
     const user = req.user;
+    const { username, password, email } = req.body;
+
+    if (username) {
+      user.username = username;
+    }
+    if (password) {
+      user.password = password;
+    }
+    if (email) {
+      user.email = email;
+    }
+
+    await user.save();
+    res.send(user);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+/* 
+^ I modified the route allow user to update data separately. I left
+the original route below. -mt
+
+app.put("/", isLoggedIn, async (req, res, next) => {
+  try {
+    const user = req.user;
     await user.update(req.body);
     res.send(user);
   } catch (ex) {
     next(ex);
   }
 });
+*/
+
